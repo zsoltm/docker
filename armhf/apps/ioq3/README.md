80ae274f7b07dfe1b6dde6e923baf8968a0c5994
Zsolt M <zsolt@meszarovics.name>
2015-04-17 00:24:10 +0200

    vanilla IOQ3 container; initial tested version

--- armhf/apps/ioq3/README.md
+++ armhf/apps/ioq3/README.md
@@ -1,34 +1,33 @@
 IO-Quake3
 =========
 
-A Quake 3 Arena client & server bult from latest [IOQuake3](http://ioquake3.org/) sources
-for ARMHF architecture.
+A Quake 3 Arena client & server bult from latest [IOQuake3](http://ioquake3.org/)
+[sources](https://github.com/zsoltm/ioq3/releases) for ARMv7l architecture.
 
 ## Usage
 
-The image does not include copyrighted game content, that means the files named `pak*.pk3`
-under `/baseq3` directory. However symlinks are created to all of these to the volume
-`/opt/q3a`. That means if you have `baseq3/pak*.pk3` files on the directory mounted to
-that volume, it sohuld run fine.
+Example command line for running a server with default settings:
 
-    opt/q3a
-    └── baseq3
-        ├── pak0.pk3
-        ├── pak1.pk3
-        ├── pak2.pk3
-        ├── pak3.pk3
-        ├── pak4.pk3
-        ├── pak5.pk3
-        ├── pak6.pk3
-        ├── pak7.pk3
-        └── pak8.pk3
-
-Example command line for running an OSP Clan Arena Server with default settings:
-
-    docker run -d\
-      -v /opt/q3a:/opt/q3a\
-      -v /mnt/docker-volumes/ioq3-ca:/home/q3\
+    docker run -dt\
+      -v `pwd`:/home/q3\
       -p 27960:27960/udp\
-      zsoltm/ioq3-armhf ioq3ded\
-        +set fs_game osp +set fs_basepath /usr/local/games/ioq3-linux-armv7l\
-        +set vm_game 2 +exec clanarena.cfg
+      --name="quake3"\
+      zsoltm/ioq3-armhf
+
+That's it, you should be able to connect from Quake 3 by entering `/connect your-host-ip` into the game console.
+
+## Known issues
+
+After an IOQ3 container is restarted, it gets a new internal IP address but Docker as of now (1.3) fails to clean up
+conntrack tables, so connecting from the outside world is impossible. A dirty workaround would be:
+
+    sudo iptables --table raw --append PREROUTING\
+     --protocol udp --source-port 27960 --destination-port 27960 --jump NOTRACK
+
+Be sure the source-port and destination-port matches your actual configuration.
+
+Another workaround is cleaning up the conntrack table manually after restart:
+
+    sudo conntrack -D -p udp
+
+Conntrack should be installed of course (`sudo apt-get install conntrack`).
--- armhf/apps/ioq3/entrypoint.sh
+++ armhf/apps/ioq3/entrypoint.sh
@@ -1,15 +1,59 @@
-#! /bin/bash
-set -e
-
+#!/bin/bash
 if [ "$1" = 'ioq3ded' ]; then
     workdir="/home/q3/.q3a"
     if [ ! -d "${workdir}" ]; then
-        mkdir -p "${workdir}"
-        chown q3:q3 "${workdir}"
+        mkdir -p "${workdir}/baseq3"
+        chown -R q3:q3 "${workdir}"
+    fi
+
+    serverCfg="${workdir}/baseq3/server.cfg"
+    if [ ! -f "${serverCfg}" ]; then
+        cat > "${serverCfg}" << EOF
+// default FFA server.cfg
+seta sv_fps 30
+seta sv_hostname "Dockerized Q3 ^2/ ^3Vanilla ^2/ ^6ARMv7l"
+seta sv_minRate "0"
+seta sv_maxclients "10"
+seta g_allowVote "0"
+seta g_forcerespawn "5"
+seta bot_nochat "1"
+seta g_gametype "0"
+seta rconPassword "" // no rcon admin allowed
+seta sv_strictauth "1"
+EOF
+        chown q3:q3 "${serverCfg}"
+    fi
+
+    mapRotationCfg="${workdir}/baseq3/map-rotation.cfg"
+    if [ ! -f "${mapRotationCfg}" ]; then
+        cat > "${mapRotationCfg}" << EOF
+// maprotation cfg
+set m1 "fraglimit 30; timelimit 0;  map q3dm17; set nextmap vstr m2"
+set m2 "fraglimit 30; timelimit 0;  map q3dm18; set nextmap vstr m3"
+set m3 "fraglimit 30; timelimit 0;  map q3dm7 ; set nextmap vstr m4"
+set m4 "fraglimit 30; timelimit 0;  map q3dm15; set nextmap vstr m5"
+set m5 "fraglimit 30; timelimit 0;  map q3dm12; set nextmap vstr m6"
+set m6 "fraglimit 30; timelimit 0;  map q3dm11; set nextmap vstr m7"
+set m7 "fraglimit 30; timelimit 0;  map q3dm9 ; set nextmap vstr m8"
+set m8 "fraglimit 30; timelimit 0;  map q3dm4 ; set nextmap vstr m9"
+set m9 "fraglimit 30; timelimit 0;  map q3dm19; set nextmap vstr m10"
+set m10 "fraglimit 30; timelimit 0; map q3dm13; set nextmap vstr m11"
+set m11 "fraglimit 30; timelimit 0; map q3dm16; set nextmap vstr m12"
+set m12 "fraglimit 30; timelimit 0; map q3dm14; set nextmap vstr m13"
+set m13 "fraglimit 30; timelimit 0; map q3dm6 ; set nextmap vstr m14"
+set m14 "fraglimit 30; timelimit 0; map q3dm3 ; set nextmap vstr m15"
+set m15 "fraglimit 30; timelimit 0; map q3dm8 ; set nextmap vstr m1"
+vstr m1
+EOF
+        chown q3:q3 "${mapRotationCfg}"
     fi
 
     shift
-    exec gosu q3 /usr/local/games/ioq3-linux-armv7l/ioq3ded $@
+    exec gosu q3 /usr/local/games/quake3/ioq3ded\
+      +set vm_game 2\
+      +set fs_basepath /usr/local/games/quake3\
+      +exec server.cfg\
+      +exec map-rotation.cfg $@
 fi
 
 exec $@
