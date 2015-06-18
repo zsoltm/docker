279495d0c53cdce8a143ed20bf0e5e21d343707e
Zsolt M <zsolt@jessie.vm>
2015-06-19 01:30:51 +0200

    added deb builder package for building src debs

--- armhf/base/buildpack-deps/build.sh
+++ armhf/base/buildpack-deps/build.sh
@@ -2,4 +2,6 @@
 
 docker build -t "zsoltm/buildpack-deps-curl-armhf:jessie" curl/\
  && docker build -t "zsoltm/buildpack-deps-scm-armhf:jessie" scm/\
- && docker build -t "zsoltm/buildpack-deps-armhf:jessie" .
+ && docker build -t "zsoltm/buildpack-deps-armhf:jessie" .\
+ && docker build -t "zsoltm/buildpack-deps-armhf:jessie" deb/
+ 
