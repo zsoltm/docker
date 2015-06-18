209f3e6a9954c5c11753807219ffa9b9f9663eb8
Zsolt M <zsolt@jessie.vm>
2015-06-19 01:58:59 +0200

    upgraded build

--- armhf/apps/nginx/build.sh
+++ armhf/apps/nginx/build.sh
@@ -1,6 +1,18 @@
 #! /bin/bash
+set -e
+
+tmpDir=`mktemp -d`
+nginxVersion=1.9.2-1
 
 docker run --rm -it\
- -v `pwd`:/mnt/out -v ${tmpDir}:/tmp/out\
- -e NGINX_VERSION=1.9.1-1~jessie\
- zsoltm/buildpack-deps-deb-armhf /mnt/out/build_deb.sh
+ -v `pwd`:/mnt/out\
+ -v ${tmpDir}:/tmp/out\
+ -e NGINX_VERSION="${nginxVersion}"'~jessie'\
+ zsoltm/buildpack-deps-deb-armhf:jessie /mnt/out/build_deb.sh
+
+cp Dockerfile ${tmpDir}
+pushd ${tmpDir}
+docker build -t zsoltm/nginx-armhf:1.9.2-2 .
+popd
+
+rm -Rf ${tmpDir}
