79fc556a024367af36d83f643daf95b79a85585a
Zsolt M <zsolt@jessie.vm>
2015-06-19 01:05:43 +0200

    changed image names; removed redundant build scripts

--- armhf/base/buildpack-deps/build.sh
+++ armhf/base/buildpack-deps/build.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-docker build -t "zsoltm/buildpack-deps:jessie-armhf" `pwd`
+docker build -t "zsoltm/buildpack-deps-curl-armhf:jessie" curl/\
+ && docker build -t "zsoltm/buildpack-deps-scm-armhf:jessie" scm/\
+ && docker build -t "zsoltm/buildpack-deps-armhf:jessie" .
