59bf4dd888651f98da5a6f9b2fc77c2f11c7e9e8
Zsolt M <zsolt@jessie.vm>
2015-06-19 01:38:50 +0200

    corrected deb builder image name

--- armhf/base/buildpack-deps/build.sh
+++ armhf/base/buildpack-deps/build.sh
@@ -3,5 +3,5 @@
 docker build -t "zsoltm/buildpack-deps-curl-armhf:jessie" curl/\
  && docker build -t "zsoltm/buildpack-deps-scm-armhf:jessie" scm/\
  && docker build -t "zsoltm/buildpack-deps-armhf:jessie" .\
- && docker build -t "zsoltm/buildpack-deps-armhf:jessie" deb/
+ && docker build -t "zsoltm/buildpack-deps-deb-armhf:jessie" deb/
  
